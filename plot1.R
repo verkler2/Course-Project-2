b2f1beee571c05f3ee2a3322c3b891ec9e6a3fa2
verkler2 <110425001+verkler2@users.noreply.github.com>
2023-09-28 02:07:12 -0400

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,9 @@
+setwd("/Users/jverk/OneDrive/Documents/testdir/specdata")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
